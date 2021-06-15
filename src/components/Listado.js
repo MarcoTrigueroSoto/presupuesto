226ccd161bf7fb3ef05a4ca48b2113b6a31ae564
MarcoTrigueroSoto <mrctrgrst@gmail.com>
2021-06-14 18:46:16 -0600

    1 try fix listado js

--- src/components/Listado.js
+++ src/components/Listado.js
@@ -8,7 +8,10 @@ const Listado = ({gastos}) => {
          
          <h2>Listado</h2> 
             {gastos.map( gasto => (
-                <p>{gasto.id} </p>
+                <Gasto 
+                key={gasto.id}    
+                gasto = {gasto}
+                />
                 
                
             ))}
